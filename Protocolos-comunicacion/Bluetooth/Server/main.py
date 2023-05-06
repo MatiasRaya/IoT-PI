97a2858df96636d5878b25128826b9ed0496f248
Matias Raya <matias.raya@mi.unc.edu.ar>
2023-05-06 11:26:19 -0300

    Se mejoro el chat y ahora es interactivo

--- Protocolos-comunicacion/Bluetooth/Server/main.py
+++ Protocolos-comunicacion/Bluetooth/Server/main.py
@@ -1,49 +1,78 @@
 from network import Bluetooth
+import gc
 import pycom
 import time
 
+# Se apaga el led de la placa
 pycom.heartbeat(False)
 
+# Se configura el nombre del dispositivo
+device_name = 'FiPy'
+
+# Se configura el handler para las conexiones
 def conn_cb (bt_o):
+    # Se obtienen los eventos
     events = bt_o.events()
+    # Se verifica si el evento es de conexión
     if  events & Bluetooth.CLIENT_CONNECTED:
-        pycom.rgbled(0x007f00) # green
+        # Se enciende el led verde de la placa
+        pycom.rgbled(0x007f00)
+        # Se imprime en consola que se ha conectado un cliente
         print("Client connected")
-        time.sleep(5)
+        # Se espera un segundo
+        time.sleep(1)
+    # Se verifica si el evento es de desconexión
     elif events & Bluetooth.CLIENT_DISCONNECTED:
-        pycom.rgbled(0x7f0000) # red
-        char1.value(67)
-        time.sleep(5)
+        # Se enciende el led rojo de la placa
+        pycom.rgbled(0x7f0000)
+        # Se imprime en consola que se ha desconectado un cliente
+        print("Client disconnected")
+        # Se espera un segundo
+        time.sleep(1)
 
+# Se configura el handler para las escrituras y lecturas
 def char1_cb_handler(chr, data):
-
-    # The data is a tuple containing the triggering event and the value if the event is a WRITE event.
-    # We recommend fetching the event and value from the input parameter, and not via characteristic.event() and characteristic.value()
+    # Se obtienen los eventos y el valor
     events, value = data
+    # Se verifica si el evento es de escritura
     if  events & Bluetooth.CHAR_WRITE_EVENT:
-        pycom.rgbled(0x00007f) # blue
+        # Se enciende el led azul de la placa
+        pycom.rgbled(0x00007f)
+        # Se imprime en consola el valor recibido
         print("Write request with value = {}".format(value))
-        time.sleep(5)
-    else:
-        pycom.rgbled(0x7f7f00) # yellow
-        print('Read request on char 1')
+        # Se esperan 10 milisegundos
+        time.sleep(0.01)
+    # Se verifica si el evento es de lectura
+    elif events & (Bluetooth.CHAR_READ_EVENT | Bluetooth.CHAR_SUBSCRIBE_EVENT):
+        # Se libera memoria
+        gc.collect()
 
-def char2_cb_handler(chr, data):
-    # The value is not used in this callback as the WRITE events are not processed.
-    events, value = data
-    if  events & Bluetooth.CHAR_READ_EVENT:
-        pycom.rgbled(0x7f3f00) # orange
-        print('Read request on char 2')
+        # Se enciende el led amarillo de la placa
+        pycom.rgbled(0x7f7f00)
+        # Se ingresa texto por consola
+        text = input("Enter your message: ")
 
+        # Se imprime por pantalla el tamaño del texto
+        print("Sending message of size {}".format(len(text)))
+
+        # Se envía el texto ingresado
+        chr1.value(text)
+        # Se esperan 10 milisegundos
+        time.sleep(0.01)
+
+# Se configura el bluetooth
 bluetooth = Bluetooth()
-bluetooth.set_advertisement(name='LoPy')
+# Se configura la antena externa
+bluetooth.init(antenna=Bluetooth.EXT_ANT)
+# Se configura el nombre del dispositivo y su fabricante
+bluetooth.set_advertisement(name=device_name, manufacturer_data="Pycom")
+# Se configura el handler para las conexiones
 bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
+# Se inicia el servicio
 bluetooth.advertise(True)
 
+# Se configura el servicio
 srv1 = bluetooth.service(uuid=0xec00, isprimary=True)
+# Se configura la característica y su handler
 chr1 = srv1.characteristic(uuid=0xec0e, value="Hola desde el server")
-char1= chr1.callback(trigger=Bluetooth.CHAR_WRITE_EVENT | Bluetooth.CHAR_READ_EVENT, handler=char1_cb_handler)
-
-# srv2 = bluetooth.service(uuid=1234, isprimary=True)
-# chr2 = srv2.characteristic(uuid=4567, value=0x1234)
-# char2_cb = chr2.callback(trigger=Bluetooth.CHAR_READ_EVENT, handler=char2_cb_handler)
+char1= chr1.callback(trigger=Bluetooth.CHAR_WRITE_EVENT | Bluetooth.CHAR_READ_EVENT | Bluetooth.CHAR_SUBSCRIBE_EVENT, handler=char1_cb_handler)

224893e7a93e5cad38041ee146096d7391631ed3
MatiasRaya <matias.raya@mi.unc.edu.ar>
2024-07-12 20:40:06 -0300

    Se agrega ejemplo de lectura en de un FTP

--- PI/main.py
+++ PI/main.py
@@ -5,6 +5,71 @@ from ntpModule import NTP
 from configModule import ConfigModule
 from ledModule import LedModule
 
+from ftpadvanced import AdvancedFTP
+
+wifi = Wifi('pruebaAP', 'STA')
+wifi.scan()
+wifi.wifi_connect('Marga', 'Milanesa6124')
+
+ftp = AdvancedFTP('192.168.1.180', 21)
+ftp.login('ftpuser', 'ftpuser')
+files = ftp.nlst()
+print(files)
+
+filename = 'prueba.txt'
+
+with open(filename, 'wb') as file:
+    ftp.retrbinary('RETR ' + filename, file.write)
+
+filename2 = 'prueba.py'
+with open(filename2, 'wb') as file:
+    ftp.retrbinary('RETR ' + filename2, file.write)
+
+ftp.quit()
+
+print('Archivo descargado')
+
+# Se copia el archivo descargado a la SD
+
+import os
+import machine
+
+# Se crea un objeto de la clase machine para acceder a la tarjeta SD
+sd = machine.SD()
+
+# Se monta la tarjeta SD
+os.mount(sd, '/sd')
+
+dirs = os.listdir('/sd')
+
+print(dirs)
+
+destination_dir = '/sd/'
+
+try:
+    with open(filename, 'rb') as src_file:
+        with open(destination_dir + filename, 'wb') as dest_file:
+            while True:
+                buf = src_file.read(1024)
+                if not buf:
+                    break
+                dest_file.write(buf)
+    print("Archivo copiado a '{}{}' exitosamente.".format(destination_dir, filename))
+except OSError as e:
+    print("No se pudo copiar el archivo: {}".format(e))
+
+try:
+    with open(filename2, 'rb') as src_file:
+        with open(destination_dir + filename2, 'wb') as dest_file:
+            while True:
+                buf = src_file.read(1024)
+                if not buf:
+                    break
+                dest_file.write(buf)
+    print("Archivo copiado a '{}{}' exitosamente.".format(destination_dir, filename2))
+except OSError as e:
+    print("No se pudo copiar el archivo: {}".format(e))
+    
 # # Se crea una instancia de la clase LedModule
 # ledModule = LedModule()
 

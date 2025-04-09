8b9ec4354d4b304e275e58229a4ef85654277a15
matiasraya <matias.raya@mi.unc.edu.ar>
2025-04-09 18:38:53 -0300

    Se eliminan archivos que no se utilizan

--- gsm/gsm.h
+++
@@ -1,9 +0,0 @@
-#include "includes/utilities.h"
-
-bool checkSimCard();
-bool connetToNetwork(const char* apn);
-String sendATCommand(const char* command, uint32_t timeout);
-int getSignalQuality();
-String getLocalIP();
-bool sendHttpGetRequest();
-bool sendHttpPostRequest();

7643e2b80763129469a1d8fd97fed3f635dfce14
matiasraya <matias.raya@mi.unc.edu.ar>
2025-04-09 21:05:01 -0300

    Se agrega comprobacion de inicio de SD, de carga de datos y de inicio de GSM y WiFi, en caso que GSM no funcione

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "network/network.h"
-#include "SD/sd.h"
+#include "logger/logger.h"
+#include "sd/sd.h"
+#include "config/config.h"
+#include "utilities.h"
+
+#define classNAME "main"
 
 void setup()
 {
     Serial.begin(MODEM_BAUDRATE);
 
-    Serial.println("Start Sketch");
+    // Serial.println("Start Sketch");
 
     SerialAT.begin(MODEM_BAUDRATE, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
 
@@ -26,11 +31,72 @@ void setup()
     delay(100);
     digitalWrite(BOARD_PWRKEY_PIN, LOW);
 
-    // initGSM("datos.personal.com");
-    if (initGSM()) {
-        Serial.println("GSM initialized successfully");
+    if (!initSD()) {
+        Serial.println("SD initialized failed");
+
+        enableSD(false);
+    }
+    else {
+        Serial.println("SD initialized successfully");
+        enableSD(true);
+    }
+
+    LOG_INFO(classNAME, "SD initialized successfully");
+
+    if (!LittleFS.begin()) {
+        // Serial.println("Error al montar LittleFS");
+        LOG_ERROR(classNAME, "Failed to mount LittleFS");
     } else {
-        Serial.println("GSM initialization failed");
+        LOG_INFO(classNAME, "LittleFS mounted successfully");
+    }
+
+    Config cfg = readConfig();
+    LOG_INFO(classNAME, "SN: %s, APN: %s, SSID: %s", cfg.sn.c_str(), cfg.apn.c_str(), cfg.ssid.c_str());
+
+    bool enableGSM = true, enableWiFi = true;
+
+    if (cfg.apn.length() > 0) {
+        // Serial.println("APN configured, using custom APN");
+        LOG_INFO(classNAME, "APN configured, using custom APN");
+
+        enableGSM = initGSM(cfg.apn.c_str());
+    } else {
+        // Serial.println("APN not configured, using default APN");
+        LOG_INFO(classNAME, "APN not configured, using default APN");
+
+        enableGSM = initGSM();
+    }
+
+    if (enableGSM) {
+        // Serial.println("GSM initialized successfully");
+        LOG_INFO(classNAME, "GSM initialized successfully");
+    } else {
+        // Serial.println("GSM initialization failed");
+        LOG_ERROR(classNAME, "GSM initialization failed");
+
+        enableGSM = false;
+
+        enableWiFi = initWiFi(cfg.ssid.c_str(), cfg.psk.c_str());
+
+        if (cfg.ssid.length() > 0 && cfg.psk.length() > 0) {
+            // Serial.println("WiFi configured, using custom SSID and PSK");
+            LOG_INFO(classNAME, "WiFi configured, using custom SSID and PSK");
+
+            enableWiFi = initWiFi(cfg.ssid.c_str(), cfg.psk.c_str());
+        } else {
+            // Serial.println("WiFi not configured, using default SSID and PSK");
+            LOG_INFO(classNAME, "WiFi not configured, using default SSID and PSK");
+
+            enableWiFi = initWiFi();
+        }
+
+        if (enableWiFi) {
+            // Serial.println("WiFi initialized successfully");
+            LOG_INFO(classNAME, "WiFi initialized successfully");
+        } else {
+            // Serial.println("WiFi initialization failed");
+            LOG_ERROR(classNAME, "WiFi initialization failed");
+        }
     }
 }
 
